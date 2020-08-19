d9f264dfa4157d12bab6acdb70d2a9483e393b7a
thompsondj5 <69919402+thompsondj5@users.noreply.github.com>
2020-08-19 14:06:09 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/*************************
+* Hello
+**************************
+*Deontae Thompson
+*8/19/20
+CMSC255 002
+**************************/
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("<Deontae Thompson>");
+		System.out.println("<Hello>");
+		System.out.println("<This project is teaching me how to use Java>");
+		System.out.println("<8/19/20>");
+		System.out.println("<CMSC225 002>");
+		System.out.println();
+	}
+}
+	
